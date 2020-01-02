ebd400950c58d43414fee961bc1834ede8824188
RobertWojtaszczyk <robert.wojtaszczyk@gmail.com>
2020-01-02 09:14:06 +0100

    chapter 4 sec auth in memory - ok

--- src/main/java/com/rw/spring/pizza/security/SecurityConfig.java
+++ src/main/java/com/rw/spring/pizza/security/SecurityConfig.java
@@ -1,14 +1,23 @@
 package com.rw.spring.pizza.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
+
+/*    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
@@ -19,5 +28,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("woody")
                 .password("bullseye")
                 .authorities("ROLE_USER");
+    }*/
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                //.passwordEncoder(passwordEncoder())
+                .withUser("admin")
+                //.password(passwordEncoder().encode("admin"))
+                .password("{noop}admin") // java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
+                // Błąd ten oznacza, że musisz jawnie podać, w jaki sposób jest zakodowane twoje hasło (w Spring Security 5 zmienił się format przechowywania hasła i teraz przed hasłem trzeba podać algorytm kodowania hasła). Na potrzeby testów korzystamy z inMemoryAuthentication, a dla wygody nie będziemy kodować hasła, więc potrzebujemy przed hasłem dodać {noop}
+                .authorities("ROLE_USER");
+                //.roles("USER");
+    }
+
+/*    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 }
